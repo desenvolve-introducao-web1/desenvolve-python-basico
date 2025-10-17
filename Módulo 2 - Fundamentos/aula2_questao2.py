cd8dcce063cbadfc24ee6dbf92477f423a4417dd
Daniel Desenvolvecurso <danielaugustogomen@Gmail.com>
2025-10-17 14:09:06 -0300

    Add files via upload
    Módulo 2 - Fundamentos
    
    Aula 2.2 - Tipos de Dados - Exercícios

---
+++ Módulo 2 - Fundamentos/aula2_questao2.py
@@ -0,0 +1,8 @@
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+resultado = valor1 + valor2
+print(texto, type(texto))
+print(valor1, type(valor1))
+print(valor2, type(valor2))
+print(resultado, type(resultado))
