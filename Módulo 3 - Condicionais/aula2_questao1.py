05bad9a63910c7600f1ceb8c0b4ada16f8ca046a
Daniel Desenvolvecurso <danielaugustogomen@Gmail.com>
2025-10-23 14:33:05 -0300

    Add files via upload
    Módulo 3 - Condicionais
    
    Aula 3.2 - Relacionais e Lógicas - Exercícios

---
+++ Módulo 3 - Condicionais/aula2_questao1.py
@@ -0,0 +1,6 @@
+idade_juliana = int(input("Digite a idade de Juliana: "))
+idade_cris = int(input("Digite a idade de Cris: "))
+if idade_juliana > 17 and idade_cris > 17:
+    print("True")
+else:
+    print("False")
